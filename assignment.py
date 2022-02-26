e76d3adaf5b0cf6b1457464cf4bbc89427301076
Lyn <38800761+slynj@users.noreply.github.com>
2022-02-26 17:02:04 -0500

    created basic user interface
    - mode buttons
    - inputs
    - message box
    - clear button
    - submit button
    - exit button

--- assignment.py
+++ assignment.py
@@ -1,10 +1,10 @@
 # -----------------------------------------------------------------------------
-# Name:        Assignment Template (assignment.py)
+# Name:        Number Guessing Game (guessing_game.py)
 # Purpose:     A description of your program goes here.
 #
-# Author:      Mr. Brooks
-# Created:     13-Sept-2020
-# Updated:     13-Sept-2020
+# Author:      Lyn Jeong
+# Created:     24-Feb-2022
+# Updated:     1-Mar-2022
 # -----------------------------------------------------------------------------
 # I think this project deserves a level XXXXXX because ...
 #
@@ -13,3 +13,19 @@
 #   ...
 #   ...
 # -----------------------------------------------------------------------------
+
+import PySimpleGUI as sg
+
+sg.theme('BlueMono')
+layout = [[sg.Button('Easy'), sg.Button('Medium'), sg.Button('Hard')],
+          [sg.Text('input range: '), sg.Text(size=(15, 1), key='-RANGE-')],
+          [sg.Input(key='-IN-'), sg.Button('Submit')],
+          [sg.Text(key='-MSG-')],
+          [sg.Button('Clear', key='-LEFT-'), sg.Exit()]
+          ]
+
+window = sg.Window('Number Guessing Game', layout)
+mode = ''
+event, values = window.read()
+if event == sg.WIN_CLOSED or event == 'Exit':
+    window.close()
