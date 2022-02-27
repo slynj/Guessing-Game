b2c0522776aa78673a93f6a43954c7344dabfb5b
Lyn <38800761+slynj@users.noreply.github.com>
2022-02-26 22:05:38 -0500

    added reset button
    - resets the game

--- assignment.py
+++ assignment.py
@@ -18,7 +18,8 @@ import PySimpleGUI as sg
 import random
 
 sg.theme('BlueMono')
-layout = [[sg.Button('Easy'), sg.Button('Medium'), sg.Button('Hard')],
+layout = [[sg.Button('Reset')],
+          [sg.Button('Easy'), sg.Button('Medium'), sg.Button('Hard')],
           [sg.Text('input range: '), sg.Text(key='-RANGE-')],
           [sg.Input(key='-IN-', justification='center'), sg.Button('Submit')],
           [sg.Text(key='-MSG-')],
@@ -63,7 +64,7 @@ def modeSelect():  # sets the mode according to user input
         elif event == 'Hard':
             mode = 'Hard'
             end = 100
-        
+
         else:
             window['-MSG-'].update('Please Select a Mode')
             noSkip = False
@@ -77,37 +78,45 @@ def compareNum():  # gets user input and compares it to the number chosen
     global userNum, counter, num, status  # stores user inputted number
 
     if event == 'Submit':
-        userNum = values['-IN-']  # get the input
-        counter += 1  # counts the number of tries
 
-        if not userNum.isnumeric():
-            if '-' in userNum:
-                window['-MSG-'].update(f"The range is from 0-{end}")
-
-            else:
-                window['-MSG-'].update("Please Input a Number")
+        if mode == '':
+            window['-MSG-'].update('Please Select a Mode')
 
         else:
-            userNum = int(values['-IN-'])
+            userNum = values['-IN-']  # get the input
+            counter += 1  # counts the number of tries
 
-            # Check if the number is out of the range
-            if userNum > end:
-                window['-MSG-'].update(f"The range is from 0 - {end}")
+            if not userNum.isnumeric():
+
+                if '-' in userNum:
+                    window['-MSG-'].update(f"The range is from 0-{end}")
+
+                else:
+                    window['-MSG-'].update("Please Input a Number")
 
             else:
-                # Check if the number is big/small/or the same
-                if userNum > num:
-                    window['-MSG-'].Update('')
-                    window['-MSG-'].update("Try a smaller number!")
-                elif userNum < num:
-                    window['-MSG-'].Update('')
-                    window['-MSG-'].update("Try a bigger number!")
+                userNum = int(values['-IN-'])
+
+                # Check if the number is out of the range
+                if userNum > end:
+                    window['-MSG-'].update(f"The range is from 0 - {end}")
+
                 else:
-                    window['-MSG-'].Update('')
-                    window['-MSG-'].update(f"Congratulations! You guessed the number in {counter} tries")
-                    window['-RETRY-'].update("Would you like to replay?")
-                    window['-LEFT-'].update("Replay")
-                    status = "finished"
+                    # Check if the number is big/small/or the same
+                    if userNum > num:
+                        window['-MSG-'].update(text_color='brown')
+                        window['-MSG-'].update("Try a smaller number!")
+
+                    elif userNum < num:
+                        window['-MSG-'].update(text_color='red')
+                        window['-MSG-'].update("Try a bigger number!")
+
+                    else:
+                        window['-MSG-'].update(text_color='white')
+                        window['-MSG-'].update(f"Congratulations! You guessed the number in {counter} tries")
+                        window['-RETRY-'].update("Would you like to replay?")
+                        window['-LEFT-'].update("Replay")
+                        status = "finished"
 
 
 def replayGame():   # ask for a replay and replays/ends the game
@@ -123,6 +132,13 @@ def clearField():
         window['-IN-'].update('')
 
 
+def resetGame():
+    if event == 'Reset':
+        print('reset')
+        reset()
+        resetMsg()
+
+
 reset()
 
 while True:
@@ -132,6 +148,7 @@ while True:
     compareNum()
     replayGame()
     clearField()
+    resetGame()
     # print(event)
 
     if event == sg.WIN_CLOSED or event == 'Exit':
